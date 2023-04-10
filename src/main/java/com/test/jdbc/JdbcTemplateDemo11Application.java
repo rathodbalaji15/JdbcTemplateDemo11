ed8545288e8211ad1a63b8c988985462650c95fb
balajirathod <rathodbalaji123@gmail.com>
2023-04-10 17:53:29 +0530

    Test line for FB branch

--- src/main/java/com/test/jdbc/JdbcTemplateDemo11Application.java
+++ src/main/java/com/test/jdbc/JdbcTemplateDemo11Application.java
@@ -8,6 +8,7 @@ public class JdbcTemplateDemo11Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JdbcTemplateDemo11Application.class, args);
+		System.out.println("This is test line1 for FB branch");
 	}
 
 }
